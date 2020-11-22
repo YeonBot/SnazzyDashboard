17448fd209af422b679bab02916d5a01483f7f87
정성연 <yeonbot@jeongseong-yeon-ui-MacBookPro.local>
2020-11-22 21:07:25 +0900

    feat(stub): bookmark stub data 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-import { getTree } from './chromeAPI/bookmark';
+import {getTree} from './chromeAPI/bookmark';
 
 import './App.css';
 
 function App() {
 
-    const getBookmark = async () =>{
-        const tree = await getTree();
-        console.log(tree);
+    const getBookmark = () => {
+        getTree().then(tree => {
+            console.log(tree);
+
+        });
     }
 
-  return (
-    <div className="App">
-      <div className="App-header">
-          <Button variant="contained" color="primary" onClick={getBookmark}>
-              Hello World
-          </Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="App-header">
+                <Button variant="contained" color="primary" onClick={getBookmark}>
+                    Hello World
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
