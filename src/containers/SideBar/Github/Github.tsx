24af44bb1368df36c604cec94a8a02d24ff8f32c
정성연 <tjdds1109@naver.com>
2020-11-29 13:18:43 +0900

    feat(github): githubWidget 추가

--- src/containers/SideBar/Github/Github.tsx
+++ src/containers/SideBar/Github/Github.tsx
@@ -1,12 +1,39 @@
 import React, {Component} from 'react';
+import GitHubCalendar from 'react-github-calendar';
 
 import SideBarCard from "../../../components/SideBar/Card";
 
+import style from './Github.module.scss';
+
 class Github extends Component {
+
+    componentDidMount() {
+        let scrollDiv:HTMLElement|null = window.document.getElementById('scroll');
+        // scrollDiv.scrollLeft = scrollDiv.width;
+        if(scrollDiv){
+            scrollDiv.scrollLeft = scrollDiv.scrollWidth;
+            console.log(scrollDiv.scrollWidth);
+        }
+    }
+
     render() {
+
+        let scrollDiv:HTMLElement|null = window.document.getElementById('scroll');
+        // scrollDiv.scrollLeft = scrollDiv.width;
+        if(scrollDiv){
+            scrollDiv.scrollLeft = scrollDiv.scrollWidth;
+            console.log(scrollDiv.scrollWidth);
+        }
+
         return (
             <SideBarCard>
-                Github
+                <div id='scroll' className={style.Github__wrapper}>
+                    <div className={style.Github__scroll}>
+                    <GitHubCalendar username="yeonbot"
+                                    fontSize={14}
+                                    showTotalCount={false}/>
+                    </div>
+                </div>
             </SideBarCard>
         );
     }
