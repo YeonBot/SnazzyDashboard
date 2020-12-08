fc9eb11a274dee802a1424a1e1c04e00631d87f4
Yeonbot <tjdds1109@naver.com>
2020-12-08 22:07:51 +0900

    feat(favorite): favorite 기능 추가

--- src/containers/FavoriteCard/FavoriteCard.tsx
+++ src/containers/FavoriteCard/FavoriteCard.tsx
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
 
+import File from "../../components/Bookmark/File";
+
 import style from './FavoriteCard.module.scss';
+
+const dummySite = [
+    {
+        title: "goormIDE",
+        url: 'https://ide.goorm.io/dashboard',
+    },
+    {
+        title: "gmail",
+        url: 'https://mail.google.com/',
+    },
+    {
+        title: "Calendar",
+        url: 'https://calendar.google.com/',
+    },
+]
 
 class FavoriteCard extends Component {
     render() {
         return (
             <div className={style.FavoriteCard}>
-                FavoriteCard
+                {
+                    dummySite.map((dummy)=>(
+                        <File
+                            key={dummy.title}
+                            title={dummy.title}
+                            url={dummy.url}
+                        />
+                    ))
+                }
             </div>
         );
     }
