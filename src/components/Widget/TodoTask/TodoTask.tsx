42d984249dd07a8154fe21523c40ea0ee26716c1
yeonbot <tjdds1109@naver.com>
2022-06-15 13:00:09 +0900

    FIX(css): bootstrap css 우선순위 변경

--- src/components/Widget/TodoTask/TodoTask.tsx
+++ src/components/Widget/TodoTask/TodoTask.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { CustomInput } from 'reactstrap';
 import {Todo as TodoType} from '../../../common/type';
 
