fdfe43c16dbd449ad57dd297b82f400d62264071
yeonbot <tjdds1109@naver.com>
2022-06-15 18:36:36 +0900

    fix(todo): todo 길어졌을떄 줄바꿈 되도록 html,css 수정

--- src/components/Widget/TodoTask/TodoTask.tsx
+++ src/components/Widget/TodoTask/TodoTask.tsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { FormGroup, Label, Input } from 'reactstrap';
-import {Todo as TodoType} from '../../../common/type';
+import { Todo as TodoType } from '../../../common/type';
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faTimesCircle } from "@fortawesome/free-regular-svg-icons"
 
 import styles from './TodoTask.module.scss';
@@ -12,44 +12,50 @@ type TodoTaskProps = TodoType & {
     deleteTodo: (id: number) => void,
 }
 
-export default function TodoTask({id, title, checked, updateTodo, deleteTodo}: TodoTaskProps) {
+export default function TodoTask({ id, title, checked, updateTodo, deleteTodo }: TodoTaskProps) {
 
-    const [isHovering, setHovering ] = useState(false);
+    const [isHovering, setHovering] = useState(false);
 
     const handleUpdateClick = (e: any) => {
-        const {checked, dataset: {id}} = e.target;
+        const { checked, dataset: { id } } = e.target;
 
-        if(id && typeof checked === 'boolean') {
-            updateTodo(parseInt(id), {checked});
+        if (id && typeof checked === 'boolean') {
+            updateTodo(parseInt(id), { checked });
         }
     }
 
     const handleDeleteClick = (e: any) => {
         const { id } = e.target.closest('svg').dataset;
-        
+
         deleteTodo(parseInt(id));
     }
 
-    return <li className={styles.todoTask}
-            onMouseEnter={()=> setHovering(true)}
-            onMouseLeave={()=> setHovering(false)}>
-                <FormGroup check>
-                    <Label check>
-                        <Input 
-                        data-id={id}
-                        type="checkbox"
-                        checked={!!checked} 
-                        onClick={handleUpdateClick}
-                        /> {title}
-                    </Label>
-                </FormGroup>
-          
+    return (
+    <li className={styles.todoTask}
+        onMouseOver={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}>
+        <FormGroup check>
+            <Label check>
+                <Input
+                    data-id={id}
+                    type="checkbox"
+                    className={styles.todoTask__checkbox}
+                    checked={!!checked}
+                    onClick={handleUpdateClick}
+                />
+            </Label>
+        </FormGroup>
+        <span className={styles.todoTask__title}>
+            {title}
+        </span>
+        <div className={styles.todoTask__more}>
             {
-                isHovering && <FontAwesomeIcon 
+                isHovering && <FontAwesomeIcon
                     data-id={id}
                     onClick={handleDeleteClick}
                     className={styles.todoTask__delete_btn}
-                    icon={faTimesCircle}/>
+                    icon={faTimesCircle} />
             }
-    </li>;
+        </div>
+    </li>);
 }
